30e0687a4c4041e58455f733a958e9e5150017bd
Sylvie <sylvie@passions-app.com>
2020-02-25 10:52:24 +0100

    add first exo

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userInput = (props) => {
+  return (
+    <div>
+      <p>Hello tout le monde</p>
+      <p>Vous allez bien ?</p>
+    </div>
+  );
+};
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userOutput = () => {
+  return (
+    <div>
+      <p>Je suis un composant</p>
+    </div>
+  );
+};
+
+export default userOutput;
